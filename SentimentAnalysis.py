3e30c3a15ec7a99b95fa9257db31baa99bd0e236
Roshni <roshnins@hotmail.com>
2022-10-14 22:14:36 +0800

    Update

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,5 +1,4 @@
 # Installing and Importing Baseline Dependencies
-
 from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 from bs4 import BeautifulSoup
 import requests
@@ -9,7 +8,7 @@ from transformers import pipeline
 
 
 # Setup the Model
-
 model_name = "human-centered-summarization/financial-summarization-pegasus"
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name)
+
